fb7a3de4ac6a681819fc43b224b13c8469df037f
AmeerQatmosh <68341128+AmeerQatmosh@users.noreply.github.com>
2022-03-01 09:53:37 +0200

    Define Classes

--- Asynchronous Programming Assignment 2/Program.cs
+++ Asynchronous Programming Assignment 2/Program.cs
@@ -10,7 +10,46 @@ namespace Asynchronous_Programming_Assignment_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Asynchronous_Programming"); 
+            Console.WriteLine("Asynchronous Programming Concole Application");
         }
+
+        /*
+          Requirments: 
+           1. You need to build classes that represent the router and route.
+           2. You need to build a class that reads input from user to:
+              a.Create a new route.
+              b.Add a router to given route.
+           3. Create a class that contains two methods.
+              a.Sync method that calculates best route from available routes.
+              b.Async method that calculates best route asynchronously by calculating total time required for each route in a separate thread.
+           4. The number of routes for each route is dynamic depending on user input.
+           5. The max number of allowed parallel threads is 4, for instance if we have 3 routes only, then you need to run 3 threads only 
+              but if you have more than for 4 like 6, first you run 4 threads and once a thread is released you need to run one more and so on.
+        */
+
+        class Router
+        {
+
+        }
+
+        class Route
+        {
+
+        }
+
+        class UserInput
+        {
+
+            //a.Create a new route
+            //b.Add a router to given route
+        }
+
+        class a
+        {
+            //Method A: Sync method that calculates best route from available routes.
+            //Method B: Async method that calculates best route asynchronously by calculating total time required for each route in a separate thread.
+        }
+
+
     }
 }
