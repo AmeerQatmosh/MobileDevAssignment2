888c72b9b92edd45085fd13aaad70936600fe716
AmeerQatmosh <68341128+AmeerQatmosh@users.noreply.github.com>
2022-03-05 20:30:25 +0200

    Add Classes

--- Asynchronous Programming Assignment 2/Program.cs
+++ Asynchronous Programming Assignment 2/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections; 
 namespace Asynchronous_Programming_Assignment_2
 {
     internal class Program
@@ -11,45 +11,194 @@ namespace Asynchronous_Programming_Assignment_2
         static void Main(string[] args)
         {
             Console.WriteLine("Asynchronous Programming Concole Application");
+            UserInput userInput = new UserInput();
+            List<Route>  routes = userInput.getRoutes();
+            Console.WriteLine(routes.Count.ToString());
+            Route bestRoute = new Calculations(routes).syncCalculation();
+            Console.WriteLine("Shortest path is ");
+            bestRoute.printRouters();
+
         }
 
-        /*
-          Requirments: 
-           1. You need to build classes that represent the router and route.
-           2. You need to build a class that reads input from user to:
-              a.Create a new route.
-              b.Add a router to given route.
-           3. Create a class that contains two methods.
-              a.Sync method that calculates best route from available routes.
-              b.Async method that calculates best route asynchronously by calculating total time required for each route in a separate thread.
-           4. The number of routes for each route is dynamic depending on user input.
-           5. The max number of allowed parallel threads is 4, for instance if we have 3 routes only, then you need to run 3 threads only 
-              but if you have more than for 4 like 6, first you run 4 threads and once a thread is released you need to run one more and so on.
-        */
+        
 
-        class Router
+        public class Router
         {
+            int time;
+            public Router(int time)
+            {
+                this.time = time;
+            }
+
+            public int getTime()
+            {
+                return time;
+            }
 
         }
 
-        class Route
+        public class Route
         {
+            public int id;
+            public List<Router> routers = new List<Router>();
+            public Route(int id, List<Router> routers)
+            {
+                this.id = id; 
+                this.routers = routers;
+            }
 
+            public int getPathTime(){
+                int sum = 0;
+                foreach(Router router in routers)
+                {
+                    sum += router.getTime();
+                }
+                return sum;
+            }
+
+           
+            public void printRouters()
+            {
+                Console.WriteLine("Route time = " + getPathTime().ToString());
+                foreach(Router router in routers)
+                {
+                    Console.Write(router.getTime().ToString() + ",");                    
+                }
+               
+            }
         }
+
 
         class UserInput
         {
+         
+            public List<Route> routeArr = new List<Route>();
+            public List<Router> routersArr;
 
+            void print_menu()
+
+            {
+
+                Console.WriteLine("               ========================================================================");
+                Console.WriteLine("               | To perform the following operations, choose the operation number.    |");
+                Console.WriteLine("               ========================================================================");
+                Console.WriteLine("               | 1. Create a new route                                                |");
+                Console.WriteLine("               | 2. Add a router to given route                                        |");
+                Console.WriteLine("               ========================================================================");
+                Console.WriteLine("               | 3. Exit.                                                             |");
+                Console.WriteLine("               ========================================================================");
+                Console.WriteLine("\n");
+
+            }
+
+            public List<Route> getRoutes()
+            {
+                while (true)
+                {
+                    print_menu();
+                    int operation = Convert.ToInt32(Console.ReadLine());
+                    switch (operation)
+                    {
+
+                        case 1:
+                            Console.WriteLine("Please enter the routers of the route");
+                            getRoutesValues();
+                            break;
+
+                        case 2:
+                            addRouterToSpecificRoute();
+                            break;
+           
+                        case 3:
+                            return routeArr;
+
+                        default:
+                            Console.WriteLine("*Unknown choice, Please try again! ");
+                            break;
+
+                    }
+                }
+            }
+
+            void getRoutesValues()
+            {
+                    Array arr = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+                routersArr = new List<Router>();
+                foreach (int item in arr)
+                    {
+                        var router = new Router(item);
+                        routersArr.Add(router);
+           
+                    }
+                Route route = new Route(routeArr.Count, routersArr);
+                Console.WriteLine("new route created with id = " + route.id);
+                routeArr.Add(route);
+                
+            }
+
+            public void addRouterToSpecificRoute()
+            {
+                Console.WriteLine("Please enter the Route id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the value of router ");
+                int routerTime = Convert.ToInt32(Console.ReadLine());
+                
+                    Route item = routeArr.FirstOrDefault(o => o.id == id);
+                if (item != null)
+                {
+                    item.routers.Add(new Router(routerTime));
+
+                }
+
+            }
             //a.Create a new route
             //b.Add a router to given route
         }
 
-        class a
+        class Calculations
         {
             //Method A: Sync method that calculates best route from available routes.
             //Method B: Async method that calculates best route asynchronously by calculating total time required for each route in a separate thread.
+            //  public async Task<string> CalculateByAsync() {}
+            List<Route> routeArr = new List<Route>();
+            int min;
+            Route bestRoute;
+            public Calculations(List<Route> routes)
+            {
+                this.routeArr = routes;
+            }
+
+            public Route syncCalculation()
+            {
+                if (routeArr.Count > 0)
+                {
+                    min = routeArr[0].getPathTime();
+                    bestRoute = routeArr[0];
+                    foreach (Route route in routeArr)
+                    {
+                        var routePathTime = route.getPathTime();
+                        if (routePathTime < min)
+                        {
+
+                            min = routePathTime;
+                            bestRoute = route;
+                        }
+                    }
+                    return bestRoute;
+
+                } else {
+                    return null;
+
+                }
+            }
+
+            public void asyncCalculation()
+            {
+
+            }
+
+
         }
-
-
     }
 }
